07a83b5dc4a3e5dc7afbafba9b779909c73480be
Sam Foreman <saforem2@gmail.com>
2018-01-05 16:12:46 -0600

    updates to worm_ising_2d.cpp

--- worm_algorithm/src/worm_ising_2d.cpp
+++ worm_algorithm/src/worm_ising_2d.cpp
@@ -2,8 +2,6 @@
  *
  * @file worm_ising_2d.cpp
  *
- * @author Sam Foreman
- *
  * @brief Main method for Monte-Carlo implementation
  * of worm algorithm as defined for 2D Ising spin lattice.
  */
@@ -23,23 +21,12 @@
 std::tuple<int, int, int, int, int> bond_number(
     int i1, int i2, int *x, int *y, int L
 );
-//std::vector<int> bonds_history;
-//std::vector<int> head_history;
 
 int main()
 {
 
-    // seed random number
-    // unsigned long int seed;
-    // seed=time(NULL) + 20;
-
-    // FILE *seed_file = fopen("seeds.txt","w");
-    // fprintf(seed_file, "%.12lf\t %.12lf\n", seed, init_genrand(seed));
-    // fclose(seed_file);
-
     //read parameters from the input file
     int L;
-    //unsigned long int num_steps;
     unsigned long int num_steps;
     double T;
     double seed;
@@ -57,7 +44,6 @@ int main()
     double K = 1.0 / T, P_acc;
     double inv_K = 1.0 / K;
     int N = L * L;
-    //int tail = 0, head = 0, new_head;
     int tail = 0, head = 0, new_head;
     int nb, ibond, delta_nb, Nb = 0, Nb_tot = 0;
     unsigned long int step_num = 0, Z = 0;
@@ -78,26 +64,12 @@ int main()
 
     std::string bonds_file = std::string("../data/bonds/lattice_") + L_str 
       + std::string("/bonds_") + T_str_sub + std::string(".txt");
-      // std::string("_bonds/") + T_str_sub +
-      // std::string("_bond") + std::string(".txt");
 
     std::string num_bonds_file = std::string("../data/num_bonds/lattice_")
       + L_str + std::string("/num_bonds_") + L_str + std::string(".txt");
 
     std::string observables_file = std::string("../data/observables/lattice_")
       + L_str + std::string("/observables_") + L_str + std::string(".txt");
-    // std::string therm_params_path = std::string("DATA/thermalized_params/")
-    //    + std::string("lattice_") + L_str + std::string("/params_")
-    //    + T_str_sub + std::string("_.txt");
-
-    // std::string therm_bonds_path = std::string("./bonds/") + L_str +
-    //   std::string("_bonds_thermalized/") + T_str_sub +
-    //   std::string("_bond") + std::string(".txt");
-
-    // std::string observables_path = std::string("../data/observables/")
-    //   + std::string("lattice_") + L_str + std::string("/observables_") + L_str
-    //   + std::string(".txt");
-      // + L_str + std::string("/observables_") + L_str + std::string("_.txt");
 
 
     // build site->x and site->y tables
@@ -124,39 +96,6 @@ int main()
     }
 
 
-
-    // Read in previously saved thermalized parameters
-    // std::ifstream therm_params_in;
-    // if (!therm_params_in) {
-    //   std::cerr << "Unable to open thermalized params file.";
-    //   exit(1);
-    // }
-    // therm_params_in.open(therm_params_path);
-    // therm_params_in >> L >> T >> K >> Z >> Nb_tot;
-    // therm_params_in.close();
-
-    // therm_params_in >>
-    //   L >> "," >> T >> "," >> K >> "," >> Z_av >> ","
-    //   >> Z >> "," >> E_av >> ",", >> Nb_av >> "," >> Nb_tot >> ","
-    //   >> step_num >> therm_params_in;
-    // std::ifstream therm_bonds_in;
-    // therm_bonds_in.open(therm_bonds_path);
-    // //
-    // for (int b=0; b < N * 2; b++)
-    // {
-    //   therm_bonds_in >> b >> bonds[b];
-    //     // bonds[b] >> b >> >> therm_bonds_in;
-    //   // bonds_out << b << ", " << bonds[b] << std::endl;
-    // }
-    // therm_bonds_in.close();
-
-    // std::string seeds_path = std::string("seeds.txt");
-    // std::ofstream seeds_out;
-    // seeds_out.open(seeds_path, std::ofstream::app);
-    // seeds_out << seed << std::endl;
-    // seeds_out.close();
-
-    // int step_num = 0;
     // main Monte Carlo loop
     while(1) {
       if (tail == head) {
@@ -248,29 +187,6 @@ int main()
     num_bonds_out.open(num_bonds_file, std::ofstream::app);
     num_bonds_out << L << " " << T << " " << Nb << "\n";
     num_bonds_out.close();
-    // append observables to observables file
-    // if (write_obs)
-    // {
-    //   std::ofstream observables_out;
-    //   observables_out.open(observables_path, std::ofstream::app);
-    //   observables_out << L << " " << T << " " << K << " " << Z_av << " "
-    //     << E_av << " " << Nb_av << " "
-    //     << Nb_tot  << " " << step_num << std::endl;
-    //   observables_out.close();
-    // }
-
-    // save thermalized quantities for next run
-    // std::ofstream therm_params_out;
-    // therm_params_out.open(therm_params_path);
-    // if (!therm_params_out) {
-    //   std::cerr << "Unable to open " + therm_params_path << std::endl;
-    // }
-    // therm_params_out << L << " " << T << " "
-    //   << K << " " << Z << " " << Nb_tot;
-    // therm_params_out.close();
-    // therm_params_out << L <<  << T << "," << K << "," << Z_av << ","
-    //   << Z << "," << E_av << ",", << Nb_av << "," << Nb_tot << ","
-    //   << step_num << std::endl;
 
 } //end main
 
